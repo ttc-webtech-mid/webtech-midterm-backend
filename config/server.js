9e089713731f64014cc07104c7ebfd57bf550d70
Patipan <patipan.bo@ku.th>
2021-08-02 23:22:23 +0700

    config files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a62c15bf2874610d04034aff8b512a94'),
+    },
+  },
+});
